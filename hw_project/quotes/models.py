000ed064e23a5edaf6fa0258845d4d59ab664889
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-11 04:35:58 -0500

    initial commit

---
+++ hw_project/quotes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Author(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class Quote(models.Model):
+    pass
