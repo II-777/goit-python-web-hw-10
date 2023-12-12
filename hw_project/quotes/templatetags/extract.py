69938dfec401f65972fbf707c3ebb54f878ec67a
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-12 03:04:27 -0500

    add pagination and styles

---
+++ hw_project/quotes/templatetags/extract.py
@@ -0,0 +1,14 @@
+from django import template
+from bson.objectid import ObjectId
+from ..utils import get_mongodb
+
+register = template.Library()
+
+
+def get_author(id_):
+    db = get_mongodb()
+    author = db.authors.find_one({"_id": ObjectId(id_)})
+    return author["fullname"]
+
+
+register.filter("author", get_author)
--- hw_project/quotes/urls.py
+++ hw_project/quotes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'quotes'
+app_name = "quotes"
 
 urlpatterns = [
-    path('', views.main, name='root'),
+    path("", views.main, name="root"),
+    path("<int:page>", views.main, name="root_paginate"),
 ]
