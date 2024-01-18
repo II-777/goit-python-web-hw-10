a8a11fd3afc7e0b1b29da210faef33a73154e94a
II-777 <45958058+II-777@users.noreply.github.com>
2024-01-18 01:36:35 -0500

    add register, login, logout basic functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def register(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+@login_required
+def logout(request):
+    pass
