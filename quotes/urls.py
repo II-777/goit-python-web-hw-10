9b4d6d1cacf1c8bc05d1070caea07668085d1b07
II-777 <45958058+II-777@users.noreply.github.com>
2024-01-18 01:35:54 -0500

    add author_about page

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "quotes"
 
 urlpatterns = [
-    path('', views.main, name='root'),
+    path("", views.main, name="root"),
     path("page/<int:page>/", views.main, name="root_paginate"),
+    path("author/<int:author_id>", views.author_about, name="author_about"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
-from .utils import get_mongodb
+from django.shortcuts import get_object_or_404, render
+
+from quotes.models import Author, Quote
 
 
-# Create your views here.
 def main(request, page=1):
-    db = get_mongodb()
-    quotes = db.quotes.find()
-    page = int(page)
-    page = max(page, 1)
+    quotes = Quote.objects.all()
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
-    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
+    return render(request, "quotes/index.html", context={"quotes": quotes_on_page})
+
+
+def author_about(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    context = {
+        "fullname": author.fullname,
+        "born_date": author.born_date,
+        "born_location": author.born_location,
+        "description": author.description,
+    }
+    return render(request, "quotes/author_about.html", context)
